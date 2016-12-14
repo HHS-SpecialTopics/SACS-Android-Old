a82746f44c91b7b27e80bf1a904d35f3076df678
Christopher Lutz <lutzblox@gmail.com>
2016-12-14 10:52:37 -0500

    Update with web viewer

--- app/src/main/java/us/in/k12/sacs/sacsandroid/SplashActivity.java
+++ app/src/main/java/us/in/k12/sacs/sacsandroid/SplashActivity.java
@@ -1,11 +1,14 @@
 package us.in.k12.sacs.sacsandroid;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.onesignal.OneSignal;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -17,9 +20,17 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        OneSignal.startInit(this).init();
+
         setContentView(R.layout.activity_splash);
 
-        checkInternetConnection();
+        if(checkInternetConnection()){
+
+            Intent i = new Intent(this, WebViewActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 
     private boolean checkInternetConnection() {
