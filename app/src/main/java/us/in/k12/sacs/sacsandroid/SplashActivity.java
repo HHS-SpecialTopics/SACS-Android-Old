caa67c489901958a7e6f706b071003b77cef96f7
Christopher Lutz <lutzblox@gmail.com>
2017-01-12 11:05:14 -0500

    Add splash screen loading

--- app/src/main/java/us/in/k12/sacs/sacsandroid/SplashActivity.java
+++ app/src/main/java/us/in/k12/sacs/sacsandroid/SplashActivity.java
@@ -2,15 +2,21 @@ package us.in.k12.sacs.sacsandroid;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.onesignal.OneSignal;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -25,11 +31,56 @@ public class SplashActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash);
 
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                downloadImage();
+            }
+        });
+        t.start();
+        while (t.isAlive());
+
         if(checkInternetConnection()){
 
             Intent i = new Intent(this, WebViewActivity.class);
             startActivity(i);
             finish();
+        }
+    }
+
+    private void downloadImage(){
+        try {
+            File cDir = getApplication().getExternalFilesDir(null);
+            File cacheDir = new File(cDir.getPath() + "/cache");
+            if (!cacheDir.exists()) {
+                cacheDir.mkdirs();
+            }
+            File file = new File(cDir.getPath() + "/cache/splashscreen.png");
+            if (file.exists()) {
+                InputStream is = file.toURI().toURL().openConnection().getInputStream();
+                Bitmap bmp = BitmapFactory.decodeStream(is);
+                ImageView view = (ImageView) findViewById(R.id.sacsLogoSplash);
+                view.setImageBitmap(bmp);
+                is.close();
+            }
+
+            InputStream is = new URL("https://sacs-backend-chrisblutz.c9users.io/wp-content/uploads/2016/12/SplashScreen.png").openConnection().getInputStream();
+            if (is != null) {
+                FileOutputStream os = new FileOutputStream(file);
+
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+
+                os.close();
+                is.close();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 
